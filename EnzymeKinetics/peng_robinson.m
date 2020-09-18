bd19f90e5435e33820346b34dfa3fe8d088ed160
Aakanksha G <46394857+kraanky@users.noreply.github.com>
2020-09-18 15:14:24 +0530

    Update peng_robinson.m

--- EnzymeKinetics/peng_robinson.m
+++ EnzymeKinetics/peng_robinson.m
@@ -1,18 +1,23 @@
 
 syms x positive;
+
 R = 8.31446261815324;
+
 fprintf('Enter everything in SI units\n');
 P = input('Enter pressure: ');
 T = input('Enter temperature: ');
 Pc = input('Enter critical pressure: ');
 Tc = input('Enter critical temperature: '); 
 w = input('Enter omega: ');
+
 a = 0.45724*R^2*Tc^2/Pc;
 b = 0.07780*R*Tc/Pc;
 c = (1+(0.37464+1.5422*w-0.26992*w^2)*(1-(T/Tc)^0.5))^2;
+
 x0 = input('Enter initial volume: ');
 f = (R*T/(x-b))-(a*c/(x^2+2*x*b-b^2))-P;
 df = diff(f);
+
 error = 0.0000000001;
 for i=1:1000
      f0=vpa(subs(f,x,x0)); 
@@ -24,6 +29,7 @@ for i=1:1000
      end
      x0=y;
 end 
+
 fprintf('The Root is : %e \n',y);
 fprintf('No. of Iterations : %d\n',i);
 
